50e04d167d96451d1cbc389d96f4ab54d3098646
Zhihang-Liu <enterindex@gmail.com>
2020-06-14 17:52:53 +0800

    更新function check

--- analysis/mata_info.py
+++ analysis/mata_info.py
@@ -1,10 +1,10 @@
 from typing import List, Set, Iterable, Dict, Any, Optional, Tuple, Union
 from dataclasses import dataclass
 from functools import reduce
-from ast import AST
+from ast import AST, stmt
 from pathlib import Path
 
-from .utils import trait, Name, intersection, AnalysisError
+from .utils import trait, Name, intersection, PosInfo, AnalysisError
 
 
 @trait
@@ -38,11 +38,13 @@ class UnionType(TypeInfo):
         return set(reduce(intersection, traits_list))
 
 
+
 @trait
 class NameSpace:
-    def get_top_level(self) -> 'NameSpace': ...
-    def find_name(self, name: Name) -> Optional[NameSpace]: ...
-    def register_name(self, name: Name, value: NameSpace) -> 'NameSpace': ...
+    def get_top_level(self) -> 'Module': ...
+    def find_name(self, name: Name) -> Optional[Union[NameSpace, TypeInfo]]: ...
+    def register_name(self, name: Name, value: NameSpace): ...
+
 
 @dataclass
 class Module(NameSpace):
@@ -50,65 +52,94 @@ class Module(NameSpace):
     fullname: Name
     path: Path
     import_list: Dict[Name, NameSpace]
-    bind: Dict[Name, NameSpace]
+    bind: Dict[Name, Union[NameSpace, TypeInfo]]
 
-    def get_top_level(self) -> NameSpace:
+    def get_top_level(self) -> 'Module':
         return self
 
-    def find_name(self, name: Name) -> Optional[NameSpace]:
+    def find_name(self, name: Name) -> Optional[Union[NameSpace, TypeInfo]]:
         r = self.bind.get(name)
-        if r:
-            return r
+        if r: return r
         return self.import_list.get(name)
     
-    def register_name(self, name: Name, value: NameSpace) -> NameSpace:
+    def register_name(self, name: Name, value: NameSpace) -> None:
         self.bind[name] = value
-        return self
 
 
 @dataclass
-class Function(NameSpace):
+class Positional:
+    pos: PosInfo
+
+
+@dataclass
+class Function(NameSpace, Positional):
     name: Name
     fullname: Name
     is_async: bool
     is_pure: Optional[bool]
     # type_vars: Dict[Name, QTypeVar]
-    params: Dict[Name, Tuple[TypeInfo, Optional[AST]]] # (name: type = construct_expr, ...)
+    params: Dict[Name, TypeInfo] # (name: type = construct_expr, ...)
+    return_type: Optional[TypeInfo]
     throw: Optional[TypeInfo]
     sub_namespace: Optional[NameSpace]
-    body: List[AST]
+    body: List[stmt]
     parent: NameSpace
     top_level: Module
 
-    def get_top_level(self) -> NameSpace:
+    def get_top_level(self) -> Module:
         return self.top_level
 
-    def find_name(self, name: Name) -> Optional[NameSpace]:
+    def find_name(self, name: Name) -> Optional[Union[NameSpace, TypeInfo]]:
         r = self.params.get(name)
-        if r:
-            return SingleBind(
-                name, f'{self.fullname}.<local>.{name}',
-                r[0],
-                self, self.top_level)
+        if r: return r
         return self.parent.find_name(name)
     
-    def register_name(self, name: Name, value: NameSpace) -> NameSpace:
+    def register_name(self, name: Name, value: NameSpace) -> None:
         if self.sub_namespace is not None:
-            raise AnalysisError('', ..., self.top_level)
-        r = SingleBind(
-            name, f'{self.fullname}.<local>.{name}',
-            value,
-            self, self.top_level)
-        self.sub_namespace = r
-        return r
+            raise AnalysisError('', self.pos, self.top_level)
+        self.sub_namespace = value
 
 @dataclass
-class SingleBind(NameSpace):
+class SingleBind(NameSpace, Positional):
     name: Name
     fullname: Name
     typedef: Union[NameSpace, TypeInfo]
     parent: NameSpace
     top_level: Module
+    sub_namespace: List[NameSpace]
 
-    def get_top_level(self) -> NameSpace:
+    def get_top_level(self) -> 'Module':
         return self.top_level
+    
+    def find_name(self, name: Name) -> Optional[Union[NameSpace, TypeInfo]]:
+        '''
+        时间复杂度O(n)\n
+        EN: time complexity O(n)
+        '''
+        if self.name == name:
+            return self.typedef
+        return self.parent.find_name(name)
+    
+    def register_name(self, name: Name, value: NameSpace) -> None:
+        self.sub_namespace.append(value)
+
+@dataclass
+class Scope(NameSpace, Positional):
+    bind: Dict[Name, TypeInfo]
+    parent: NameSpace
+    top_level: Module
+    sub_namespace: List[NameSpace]
+
+    def get_top_level(self) -> 'Module':
+        return self.top_level
+    
+    def find_name(self, name: Name) -> Optional[Union[NameSpace, TypeInfo]]:
+        r = self.bind.get(name)
+        if r: return r
+        return self.parent.find_name(name)
+    
+    def register_name(self, name: Name, value: NameSpace) -> None:
+        self.sub_namespace.append(value)
+
+    def bind_typedef(self, name: Name, value: TypeInfo):
+        self.bind[name] = value
